c25946fc3e28dfb4293396c0aed8aaa8973c5f9c
Berti Emanuele <berti11742@itiseveripadova.edu.it>
2026-03-05 10:30:58 +0100

    feat:random

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,11 +1,13 @@
 export function Button(props: {
   type: "submit" | "button" | "reset";
   label: string;
+  onClick?: () => void;
 }) {
   return (
     <button
-      className=" border border-gray-800 bg-gray-200 text-black "
+      className="border border-gray-800 bg-gray-200 text-black"
       type={props.type}
+      onClick={props.onClick}
     >
       {props.label}
     </button>
